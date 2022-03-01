fb9d8142caa2c3193ccaf25453b8e7f8e849f99b
hyosin-Jang <cdnnnl@gmail.com>
2022-03-01 21:29:30 +0900

    💄:: 전역 스타일 모음

---
+++ src/style/globalStyle.ts
@@ -0,0 +1,8 @@
+// 전역-스타일 모음
+import { createGlobalStyle } from 'styled-components';
+import './reset.scss';
+import { theme } from './theme';
+
+// export const GlobalStyle = createGlobalStyle
+
+// global 스타일 선언
