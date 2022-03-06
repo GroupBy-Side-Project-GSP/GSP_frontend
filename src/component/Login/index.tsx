c6dfdfedf1ebc6e4596cb58ed416dfe50d077b59
hyosin-Jang <cdnnnl@gmail.com>
2022-03-07 01:42:39 +0900

    🔥:: 디렉토리 구조 변경

--- src/component/Login/index.tsx
+++
@@ -1,4 +0,0 @@
-export { default as AuthContent } from './AuthContent';
-export { default as InputWithLabel } from './InputWithLabel';
-export { default as LoginButton } from './LoginButton';
-export { default as RightAlignedLink } from './RightAlignedLink';
