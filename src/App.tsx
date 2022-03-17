2255bb51bd2ec7e210a5d46fc8076c143e6e62da
jyejyes <p_jihye97@naver.com>
2022-03-17 15:42:23 +0900

    :sparkles: :: #8 메인페이지 추가
    메인 페이지로 사용될 페이지 추가함.

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,25 @@ import Registerpage from './page/Auth/Registerpage';
 import FindIdandPwpage from './page/Auth/FindIdandPwpage';
 import FindIdpage from './page/Auth/FindIdpage';
 import FindPwpage from './page/Auth/FindPwpage';
+import MainPage from './page/MainPage';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { GlobalStyle } from './style/globalStyle';
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/auth/login" element={<Loginpage />} />
-        <Route path="/find/login" element={<FindIdandPwpage />} />
-        <Route path="/find/id" element={<FindIdpage />} />
-        <Route path="/find/pw" element={<FindPwpage />} />
-        <Route path="/auth/register" element={<Registerpage />} />
-      </Routes>
-    </Router>
+    <>
+      <GlobalStyle />
+      <Router>
+        <Routes>
+          <Route path="/auth/login" element={<Loginpage />} />
+          <Route path="/find/login" element={<FindIdandPwpage />} />
+          <Route path="/find/id" element={<FindIdpage />} />
+          <Route path="/find/pw" element={<FindPwpage />} />
+          <Route path="/auth/register" element={<Registerpage />} />
+          <Route path="/main" element={<MainPage />} />
+        </Routes>
+      </Router>
+    </>
   );
 };
 
